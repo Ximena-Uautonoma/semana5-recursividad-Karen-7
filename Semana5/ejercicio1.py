87f7361c3e7f119c6ad5809a9c2bc8ac34169329
Karen-7 <karen.perez3@cloud.uautonoma.cl>
2026-04-06 18:24:24 +0000

    Ejercicio1

--- Semana5/ejercicio1.py
+++ Semana5/ejercicio1.py
@@ -10,8 +10,7 @@ def contar_ciclo(n):
     """
     Retorna una lista con los números desde 1 hasta n usando iteración.
     """
-    # Escriba aquí su solución y borre la palabra pass de acontinuación
-    pass
+#prueba
 
 
 def contar_recursivo(n):
